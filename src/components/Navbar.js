d279852ca44131e6f0554fd324456ffbd6731dee
Jan Niklas Marzahl <janniklasmarzahl@gmail.com>
2022-05-26 23:45:07 +0200

    toast finished

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,21 @@
+import { async } from "@firebase/util";
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { UserAuth } from "../context/AuthContext";
 
 const Navbar = () => {
+  const { user, logOut } = UserAuth();
+  const navigate = useNavigate();
+
+  const handleLogOut = async () => {
+    try {
+      await logOut();
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="flex items-center justify-between p-6 z-[100] absolute w-full">
       <Link to="/">
@@ -9,16 +23,30 @@ const Navbar = () => {
           {"netflix".toUpperCase()}
         </h1>
       </Link>
-      <div>
-        <Link to="login">
-          <button className="text-white pr-4">Sign in</button>
-        </Link>
-        <Link to="signup">
-          <button className="bg-red-600 px-6 py-2 rounded-lg text-white cursor-pointer">
-            Sign up
+      {user?.email ? (
+        <div>
+          <Link to="/acount">
+            <button className="text-white pr-4">Account</button>
+          </Link>
+          <button
+            onClick={handleLogOut}
+            className="bg-red-600 px-6 py-2 rounded-lg text-white cursor-pointer"
+          >
+            Log Out
           </button>
-        </Link>
-      </div>
+        </div>
+      ) : (
+        <div>
+          <Link to="/login">
+            <button className="text-white pr-4">Sign in</button>
+          </Link>
+          <Link to="/signup">
+            <button className="bg-red-600 px-6 py-2 rounded-lg text-white cursor-pointer">
+              Sign up
+            </button>
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -1,45 +1,70 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { UserAuth } from "../context/AuthContext";
+import toast, { Toaster } from "react-hot-toast";
 
-const LogIn = () => {
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const { user, logIn } = UserAuth();
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+    try {
+      await logIn(email, password);
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+      setError(error.message);
+      toast.error("something went wrong!");
+    }
+  };
+
   return (
-    <div className="w-full h-screen>">
+    <div className="w-full h-screen">
+      <Toaster />
       <img
         className="hidden sm:block absolute w-full h-full object-cover"
-        src="https://assets.nflxext.com/ffe/siteui/vlv3/970e664f-2df4-47ce-b4fa-446082f5abc1/b316950d-5137-42d2-a5be-0bffd88bb2ff/DE-de-20220523-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+        src="https://assets.nflxext.com/ffe/siteui/vlv3/f841d4c7-10e1-40af-bcae-07a3f8dc141a/f6d7434e-d6de-4185-a6d4-c77a2d08737b/US-en-20220502-popsignuptwoweeks-perspective_alpha_website_medium.jpg"
         alt="/"
       />
-      <div className=" bg-black/60 fixed top-0 left-0 w-full h-screen "></div>
+      <div className="bg-black/60 fixed top-0 left-0 w-full h-screen"></div>
       <div className="fixed w-full px-4 py-24 z-50">
-        <div className="max-w-[450px] h-[600px] mx-auto bg-black/75 text-white ">
+        <div className="max-w-[450px] h-[600px] mx-auto bg-black/75 text-white">
           <div className="max-w-[320px] mx-auto py-16">
-            <h1 className="text-3xl font-bold ">Log In</h1>
-            <form className="w-full flex flex-col py-4">
+            <h1 className="text-3xl font-bold">Sign In</h1>
+            {error ? <p className="p-3 bg-red-400 my-2">{error}</p> : null}
+            <form onSubmit={handleSubmit} className="w-full flex flex-col py-4">
               <input
-                className="p-3 my-2 bg-gray-700 rounded-sm"
+                onChange={(e) => setEmail(e.target.value)}
+                className="p-3 my-2 bg-gray-700 rouded"
                 type="email"
-                placeholder="Your Email..."
+                placeholder="Email"
                 autoComplete="email"
               />
               <input
-                className="p-3 my-2 bg-gray-700 rounded-sm"
+                onChange={(e) => setPassword(e.target.value)}
+                className="p-3 my-2 bg-gray-700 rouded"
                 type="password"
-                placeholder="Your Password..."
+                placeholder="Password"
+                autoComplete="current-password"
               />
-              <button className="bg-red-600 py-3 my-6 rounded-lg font-bold">
-                Log In
+              <button className="bg-red-600 py-3 my-6 rounded font-bold">
+                Sign In
               </button>
-              <div className="flex items-center justify-between text-sm text-gray-500">
+              <div className="flex justify-between items-center text-sm text-gray-600">
                 <p>
-                  <input className="mr-2" type="checkbox" /> Remember me
+                  <input className="mr-2" type="checkbox" />
+                  Remember me
                 </p>
                 <p>Need Help?</p>
               </div>
-              <p className="py-8 text-gray-500">
-                <span>Dont have a account for Netflix?</span>{" "}
-                <Link className="hover:text-white" to="/signup">
-                  Sign Up
-                </Link>
+              <p className="py-8">
+                <span className="text-gray-600">New to Netflix?</span>{" "}
+                <Link to="/signup">Sign Up</Link>
               </p>
             </form>
           </div>
@@ -49,4 +74,4 @@ const LogIn = () => {
   );
 };
 
-export default LogIn;
+export default Login;
